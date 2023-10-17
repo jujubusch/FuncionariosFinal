9d3b7b18bb7af53ddcd36c9d0661a15d8053ad74
jujubusch <buschanajulia@gmail.com>
2023-10-17 16:46:38 -0300

    Criação do commit de ponto.

---
+++ Controllers/EscalaController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoFuncionarios.Models;
+
+namespace FuncionariosFinal.Controllers
+{
+    public class EscalaController : Controller
+    {
+        private readonly Contexto _context;
+
+        public EscalaController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Escala
+        public async Task<IActionResult> Index()
+        {
+              return _context.Escala != null ? 
+                          View(await _context.Escala.ToListAsync()) :
+                          Problem("Entity set 'Contexto.Escala'  is null.");
+        }
+
+        // GET: Escala/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Escala == null)
+            {
+                return NotFound();
+            }
+
+            var escala = await _context.Escala
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (escala == null)
+            {
+                return NotFound();
+            }
+
+            return View(escala);
+        }
+
+        // GET: Escala/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Escala/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,HorarioEntrada,IntervaloSaida,IntervaloRetorno,HorarioSaida")] Escala escala)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(escala);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(escala);
+        }
+
+        // GET: Escala/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Escala == null)
+            {
+                return NotFound();
+            }
+
+            var escala = await _context.Escala.FindAsync(id);
+            if (escala == null)
+            {
+                return NotFound();
+            }
+            return View(escala);
+        }
+
+        // POST: Escala/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,HorarioEntrada,IntervaloSaida,IntervaloRetorno,HorarioSaida")] Escala escala)
+        {
+            if (id != escala.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(escala);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EscalaExists(escala.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(escala);
+        }
+
+        // GET: Escala/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Escala == null)
+            {
+                return NotFound();
+            }
+
+            var escala = await _context.Escala
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (escala == null)
+            {
+                return NotFound();
+            }
+
+            return View(escala);
+        }
+
+        // POST: Escala/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Escala == null)
+            {
+                return Problem("Entity set 'Contexto.Escala'  is null.");
+            }
+            var escala = await _context.Escala.FindAsync(id);
+            if (escala != null)
+            {
+                _context.Escala.Remove(escala);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EscalaExists(int id)
+        {
+          return (_context.Escala?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/PontoController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoFuncionarios.Models;
+
+namespace FuncionariosFinal.Controllers
+{
+    public class PontoController : Controller
+    {
+        private readonly Contexto _context;
+
+        public PontoController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Ponto
+        public async Task<IActionResult> Index()
+        {
+            var contexto = _context.Ponto.Include(p => p.Funcionario);
+            return View(await contexto.ToListAsync());
+        }
+
+        // GET: Ponto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Ponto == null)
+            {
+                return NotFound();
+            }
+
+            var ponto = await _context.Ponto
+                .Include(p => p.Funcionario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ponto == null)
+            {
+                return NotFound();
+            }
+
+            return View(ponto);
+        }
+
+        // GET: Ponto/Create
+        public IActionResult Create()
+        {
+            ViewData["FuncionarioId"] = new SelectList(_context.Funcionário, "Id", "Id");
+            return View();
+        }
+
+        // POST: Ponto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,HorarioEntrada,SaidaAlmoco,VoltaAlmoco,HorarioSaida,Data,FuncionarioId")] Ponto ponto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ponto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FuncionarioId"] = new SelectList(_context.Funcionário, "Id", "Id", ponto.FuncionarioId);
+            return View(ponto);
+        }
+
+        // GET: Ponto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Ponto == null)
+            {
+                return NotFound();
+            }
+
+            var ponto = await _context.Ponto.FindAsync(id);
+            if (ponto == null)
+            {
+                return NotFound();
+            }
+            ViewData["FuncionarioId"] = new SelectList(_context.Funcionário, "Id", "Id", ponto.FuncionarioId);
+            return View(ponto);
+        }
+
+        // POST: Ponto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,HorarioEntrada,SaidaAlmoco,VoltaAlmoco,HorarioSaida,Data,FuncionarioId")] Ponto ponto)
+        {
+            if (id != ponto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ponto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PontoExists(ponto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FuncionarioId"] = new SelectList(_context.Funcionário, "Id", "Id", ponto.FuncionarioId);
+            return View(ponto);
+        }
+
+        // GET: Ponto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Ponto == null)
+            {
+                return NotFound();
+            }
+
+            var ponto = await _context.Ponto
+                .Include(p => p.Funcionario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ponto == null)
+            {
+                return NotFound();
+            }
+
+            return View(ponto);
+        }
+
+        // POST: Ponto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Ponto == null)
+            {
+                return Problem("Entity set 'Contexto.Ponto'  is null.");
+            }
+            var ponto = await _context.Ponto.FindAsync(id);
+            if (ponto != null)
+            {
+                _context.Ponto.Remove(ponto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PontoExists(int id)
+        {
+          return (_context.Ponto?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
