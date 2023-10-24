916577f53f93ea00daf53a8410ac154ef37c6d23
PedrooL77 <pedrosenai777@gmail.com>
2023-10-24 14:21:15 -0300

    corrigindo erro

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using FuncionariosFinal.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,8 +10,8 @@ builder.Services.AddControllersWithViews();
 /*builder.Services.AddDbContext<Contexto> //Ana Júlia Busch
     (options => options.UseSqlServer("Data Source=SP-1491020\\SQLSENAI;Initial Catalog = FuncionariosFinal;Integrated Security = True;TrustServerCertificate = True"));*/
 
-builder.Services.AddDbContext<Contexto> //Kauan
-    (options => options.UseSqlServer("Data Source=SP-1491006\\SQLSENAI;Initial Catalog = FuncionariosFinal;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Kauan
+    (options => options.UseSqlServer("Data Source=SP-1491006\\SQLSENAI;Initial Catalog = FuncionariosFinal;Integrated Security = True;TrustServerCertificate = True"));*/
 
 /*builder.Services.AddDbContext<Contexto> //Pedro Lucas
     (options => options.UseSqlServer("Data Source=SP-1491032\\SQLSENAI;Initial Catalog = FuncionariosFinal;Integrated Security = True;TrustServerCertificate = True"));*/
